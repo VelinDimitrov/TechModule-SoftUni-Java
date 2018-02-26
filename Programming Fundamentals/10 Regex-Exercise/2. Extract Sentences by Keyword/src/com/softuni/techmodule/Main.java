d8969b5d4b7025cd4b6f0f52c6343552a680438d
VelinDimitrov <bgruller@abv.bg>
2018-02-26 12:01:24 +0200

    Correcting exercise 2 from Regex now it gives 100/100 on the test before 20/100

--- Programming Fundamentals/10 Regex-Exercise/2. Extract Sentences by Keyword/src/com/softuni/techmodule/Main.java
+++ Programming Fundamentals/10 Regex-Exercise/2. Extract Sentences by Keyword/src/com/softuni/techmodule/Main.java
@@ -9,13 +9,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String regexWord = input.nextLine();
-	String[] sentence=input.nextLine().split("[\\!\\.\\?] ");
+        String[] sentence = input.nextLine().split("[\\!\\.\\?]");
         String regex = "\\b" + regexWord + "\\b";
         Pattern pattern = Pattern.compile(regex);
         for (int i = 0; i < sentence.length; i++) {
-            Matcher match=pattern.matcher(sentence[i]);
+            Matcher match = pattern.matcher(sentence[i].trim());
             if (match.find()) {
-                System.out.println(sentence[i]);
+                System.out.println(sentence[i].trim());
             }
         }
 
