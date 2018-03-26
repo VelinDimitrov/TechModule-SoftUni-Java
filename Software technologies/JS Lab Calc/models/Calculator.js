12c07b6e00e9e3a2612afba2caeef9e6b9628325
Velin Dimitrov <bgrullerbg@abv.bg>
2018-03-26 09:33:08 +0300

    Adding extra functionality to Basic calc

--- Software technologies/JS Lab Calc/models/Calculator.js
+++ Software technologies/JS Lab Calc/models/Calculator.js
@@ -2,9 +2,12 @@ function Calculator(leftOperand,operator,rightOperand){
     this.leftOperand=leftOperand;
     this.operator=operator;
     this.rightOperand=rightOperand;
-    
+
     this.calculateResult=function () {
         let result=0;
+        if (isNaN(this.leftOperand)||isNaN(this.rightOperand)) {
+            return 'Invalid Input';
+        }
         switch (this.operator) {
             case '+':
                     result=this.leftOperand+this.rightOperand;
@@ -17,6 +20,9 @@ function Calculator(leftOperand,operator,rightOperand){
                 break;
             case '*':
                 result=this.leftOperand*this.rightOperand;
+                break;
+            case'pow':
+                result=Math.pow(this.leftOperand*this.rightOperand,2);
                 break;
         }
         return result;
